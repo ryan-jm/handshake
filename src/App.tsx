22d095e55e23ec62585c246571544eaa7d323bd5
sanctumlysis <sanctum.lysis-0e@icloud.com>
2022-02-03 15:35:42 +0000

    App setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 import "@elastic/eui/dist/eui_theme_dark.css";
 import { EuiProvider } from "@elastic/eui";
+import { Routes, Route } from "react-router-dom";
+import Home from "./containers/Home";
+import Login from "./containers/Login";
+import Header from "./components/Header";
+import Error from "./containers/Error";
+import "./styles/globals.scss";
 
 function App() {
   return (
     <EuiProvider colorMode="dark">
-      <div className="App"></div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
     </EuiProvider>
   );
 }
